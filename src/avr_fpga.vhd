f2f06135c49c7d80bd4a5284683ed9317ec48536
Mauri Mustonen <mauri_mustonen@hotmail.com>
2018-04-15 18:30:20 +0300

    analyze problems fixed

--- src/avr_fpga.vhd
+++ src/avr_fpga.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
 
 
 entity avr_fpga is
@@ -10,7 +9,7 @@ entity avr_fpga is
     rst_n         : in  std_logic;
     switch_in     : in  std_logic_vector(8 downto 0);
     rx_in         : in  std_logic;
-    7_segment_out : out std_logic_vector(6 downto 0);
+    seven_seg_out : out std_logic_vector(6 downto 0);
     leds_out      : out std_logic_vector(3 downto 0);
     tx_out        : out std_logic
   );
@@ -44,7 +43,7 @@ architecture behavioral of avr_fpga is
       we_io_in      : in  std_logic;
       switch_in     : in  std_logic_vector(7 downto 0);
       rx_in         : in  std_logic;
-      7_segment_out : out std_logic_vector(6 downto 0);
+      seven_seg_out : out std_logic_vector(6 downto 0);
       d_out         : out std_logic_vector(7 downto 0);
       int_vec_out   : out std_logic_vector(5 downto 0);
       leds_out      : out std_logic_vector(1 downto 0);
@@ -58,13 +57,12 @@ architecture behavioral of avr_fpga is
       rst_n         : in  std_logic;
       opc_in        : in  std_logic_vector(15 downto 0);
       pc_in         : in  std_logic_vector(15 downto 0);
-      7_segment_out : out std_logic_vector(6 downto 0)
+      seven_seg_out : out std_logic_vector(6 downto 0)
     );
   end component;
 
   -- Signals for connections between blocks.
   signal int_vec_io_cpu : std_logic_vector(5 downto 0);
-  signal d_io_cpu : std_logic_vector(7 downto 0);
   signal opc_cpu_7seg : std_logic_vector(15 downto 0);
   signal pc_cpu_7seg : std_logic_vector(15 downto 0);
   signal d_cpu_io : std_logic_vector(7 downto 0);
@@ -75,8 +73,8 @@ architecture behavioral of avr_fpga is
   signal tx_io : std_logic;
 
   -- Signals for inner logic connections and logic.
-  signal 7segment_io : std_logic_vector(15 downto 0);
-  signal 7segment_7seg : std_logic_vector(15 downto 0);
+  signal seven_seg_io : std_logic_vector(6 downto 0);
+  signal seven_seg_7seg : std_logic_vector(6 downto 0);
 
 
 begin
@@ -90,7 +88,7 @@ begin
     opc_out    => opc_cpu_7seg,
     pc_out     => pc_cpu_7seg,
     d_out      => d_cpu_io,
-    adr_io_out => adr_cpu_io
+    adr_io_out => adr_cpu_io,
     rd_io_out  => rd_cpu_io,
     we_io_out  => we_cpu_io
   );
@@ -105,24 +103,24 @@ begin
     we_io_in      => we_cpu_io,
     switch_in     => switch_in(7 downto 0),
     rx_in         => rx_in,
-    7_segment_out => 7segment_io,
-    d_out         => d_io_cpu
+    seven_seg_out => seven_seg_io,
+    d_out         => d_io_cpu,
     int_vec_out   => int_vec_io_cpu,
     leds_out      => leds_out(1 downto 0),
     tx_out        => tx_io
   );
 
   segment_1 : seven_segment
-  port (
+  port map (
     clk           => clk,
     rst_n         => rst_n,
     opc_in        => opc_cpu_7seg,
     pc_in         => pc_cpu_7seg,
-    7_segment_out => 7segment_7seg
+    seven_seg_out => seven_seg_7seg
   );
 
   leds_out(2) <= tx_io;
   leds_out(3) <= rx_in;
-  7_segment_out <= 7segment_io when switch_in(8) = '1' else 7segment_7seg;
+    seven_seg_out <= seven_seg_io when switch_in(8) = '1' else seven_seg_7seg;
 
 end architecture;
